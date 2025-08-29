29853cadb9565fbd7d20db99ee069a650e715d1f
Joshua-Ogbonna <devjaykes@gmail.com>
2025-08-29 23:55:01 +0100

    chore: work

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   apps : [{
-    script: 'npm start',
+    name: 'ministry-of-works',
+    script: 'npm',
+    args: 'start',
+    cwd: '/home/ubuntu/source',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
   }],
+
   deploy : {
     production : {
       key: 'abeg.pem',
@@ -11,8 +23,8 @@ module.exports = {
       repo : 'git@github.com:IDCLofficial/ministry-of-works.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
+      'pre-setup': 'rm -rf /home/ubuntu/source',
       'post-deploy' : 'source ~/.nvm/nvm.sh && export NEXT_PUBLIC_CONTENTFUL_SPACE_ID=g08mvfhb24ji && export NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN=yZmq2UuZ1_8XDAKpc6zdstd8XtD4vEwTc5aqK-zPU-o && export NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN=rlJ3RsWYOuYx4jdCjveT_2w5ekUNt4e14JuCHCdRfsw && export NEXT_PUBLIC_MINISTRY_ID=5bIShDqyccqxAFMrjoJXZQ && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
   }
